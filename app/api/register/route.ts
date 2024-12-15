8299a08abb135ab77fb76e2f97f012c2c818d62d
Yasmani Palmero <yasmani.palmero@ntsprint.com>
2024-12-14 21:16:17 -0500

    Refactor registration API: streamline POST method by removing unnecessary method checks and improving user existence verification

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,46 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
-import User from "@/models/User"; // Import Mongoose User model
+import User from "@/models/User";
 
-export default async function handler(req: NextRequest) {
-  const { method } = req;
+export async function POST(req: NextRequest) {
+  const { name, phone, password, isAdmin } = await req.json();
 
-  try {
-    switch (method) {
-      case "POST":
-        const { name, phone, password, isAdmin } = await req.json();
-        const hashedPassword = password ? bcrypt.hashSync(password, 10) : "";
-
-        // verify if user exists
-        const exists = await User.findOne({ phone });
-        if (exists) {
-          return NextResponse.json(
-            { error: "User already exists" },
-            { status: 400 }
-          );
-        }
-
-        const user = new User({
-          name,
-          phone,
-          password: hashedPassword,
-          isAdmin,
-        });
-
-        await user.save();
-        return NextResponse.json(user, { status: 200 });
-
-      default:
-        return NextResponse.json(
-          { error: `Method ${method} Not Allowed` },
-          { status: 405, headers: { Allow: "POST" } }
-        );
-    }
-  } catch (error) {
-    console.error("Error in handler function:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+  // verify if user exists
+  const exists = await User.findOne({ phone });
+  if (exists) {
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
   }
+
+  const user = new User({
+    name,
+    phone,
+    password,
+    isAdmin,
+  });
+
+  await user.save();
+  return NextResponse.json(user, { status: 200 });
 }

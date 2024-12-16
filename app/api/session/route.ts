a21739369bce51c5816193bd2b9db87a78a07373
Yasmani Palmero <yasmani.palmero@ntsprint.com>
2024-12-16 09:40:46 -0500

    Refactor session API handler: simplify GET method by removing unnecessary switch statement and improving error handling

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -1,49 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/User"; // Import Mongoose User model
 
-export default async function handler(req: NextRequest) {
-  const { method } = req;
+export async function GET(req: NextRequest) {
+  const session = req.cookies.get("session");
 
-  try {
-    switch (method) {
-      case "GET":
-        // get session from cookies
-        const session = req.cookies.get("session");
-
-        if (!session) {
-          return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-        }
-        const {
-          user: { id },
-        } = JSON.parse(session.value);
-        const user = await User.findById(id);
-
-        if (!user) {
-          return NextResponse.json(
-            { error: "User not found" },
-            { status: 404 }
-          );
-        }
-
-        return NextResponse.json({
-          _id: user._id,
-          name: user.name,
-          phone: user.phone,
-          isAdmin: user.isAdmin,
-          appointments: user.appointments,
-        });
-
-      default:
-        return NextResponse.json(
-          { error: `Method ${method} Not Allowed` },
-          { status: 405, headers: { Allow: "GET" } }
-        );
-    }
-  } catch (error) {
-    console.error("Error in handler function:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
+  const {
+    user: { id },
+  } = JSON.parse(session.value);
+  const user = await User.findById(id);
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json(user);
 }

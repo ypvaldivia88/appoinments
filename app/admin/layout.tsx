0495792770057fc628d3a3350eeaa5e65234eaab
Yasmani Palmero <yasmani.palmero@ntsprint.com>
2024-12-19 15:03:33 -0500

    Refactor layout components: add click outside detection for Sidebar and Modal to improve user experience

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,20 +1,38 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { ReactNode, useState, useEffect, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 import Sidebar from "@/app/components/Sidebar";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   const [activeLink, setActiveLink] = useState("users");
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        setIsNavOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sidebarRef]);
 
   return (
     <div className="flex min-h-screen">
-      <Sidebar
-        isNavOpen={isNavOpen}
-        activeLink={activeLink}
-        setActiveLink={setActiveLink}
-      />
+      <div ref={sidebarRef}>
+        <Sidebar
+          isNavOpen={isNavOpen}
+          activeLink={activeLink}
+          setActiveLink={setActiveLink}
+        />
+      </div>
       <main
         className="flex-1 bg-gray-700 ml-0"
         style={{ width: "calc(100% - 16rem)" }}
--- app/components/GenericForm.tsx
+++ app/components/GenericForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Modal from "@/app/components/Modal";
 
 interface GenericFormProps {
@@ -14,26 +14,45 @@ export default function GenericForm({
   onClose,
   children,
 }: GenericFormProps) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [modalRef]);
+
   return (
     <Modal title={title} onClose={onClose}>
-      <form onSubmit={onSubmit}>
-        {children}
-        <div className="flex justify-end">
-          <button
-            type="button"
-            className="bg-gray-500 text-white font-bold py-2 px-4 rounded-full shadow-lg hover:bg-gray-700 transition-colors mr-2"
-            onClick={onClose}
-          >
-            Cancelar
-          </button>
-          <button
-            type="submit"
-            className="font-bold py-2 px-4 rounded-full shadow-lg bg-purple-600 text-white hover:bg-purple-400 transition-colors w-full"
-          >
-            Guardar
-          </button>
-        </div>
-      </form>
+      <div ref={modalRef}>
+        <form onSubmit={onSubmit}>
+          {children}
+          <div className="flex justify-end">
+            <button
+              type="button"
+              className="bg-gray-500 text-white font-bold py-2 px-4 rounded-full shadow-lg hover:bg-gray-700 transition-colors mr-2"
+              onClick={onClose}
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="font-bold py-2 px-4 rounded-full shadow-lg bg-purple-600 text-white hover:bg-purple-400 transition-colors w-full"
+            >
+              Guardar
+            </button>
+          </div>
+        </form>
+      </div>
     </Modal>
   );
 }
